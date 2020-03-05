def7a0ae7a756db73c8ace99b13756723ef5fb6f
tuo.li <tuo.li@howbuy.com>
2020-03-05 10:21:11 +0800

    增加申购费率的影响，同时删除反复申购赎回的策略。实测发现费率对盈利基本可以忽略。

--- fundvalue.py
+++ fundvalue.py
@@ -78,7 +78,7 @@ class FundValue():
         return self.init_peinfo(url)
 
     def init_f_info(self, fid):
-        """ 获取指定基金的价格 """
+        """ 获取指定基金的价格，只能获取当前净值 """
         fdict = {}
         url = 'https://danjuanapp.com/djapi/fund/nav/history/' + str(fid) + '?page=1&size=10'
         header = {}
@@ -103,7 +103,7 @@ class FundValue():
         return json.loads(re.match(r'[^(]*[(]({.*})[)][^)]*', response.content.decode('utf-8'), re.S).group(1))
 
     def init_f_info2(self, fid):
-        """ 获取指定基金的价格 """
+        """ 获取指定基金的价格，可以获取当前净值和累计净值 """
         fdict = {}
         url = 'http://api.fund.eastmoney.com/f10/lsjz?callback=jQuery&pageIndex=1&pageSize=20&startDate=&endDate=&fundCode=' + fid
         header = {}
@@ -227,12 +227,14 @@ class FundValue():
         weight = weight_pe * weight_price
 
         capital = round(base * weight, 2)
+        # 以累计净值计算购买数量，如当天购买，则不准确。
         amount = round(capital/cur_price, 2)
         #print(dt, weight, capital)
         return (capital, amount)
 
-    def buy_longtime(self, fid, begin_date, end_date, n_pe=2, n_price=4, base=100):
-        """ 长期购买一段时间，用于测试。默认买100块钱。以最后一天价格为基准计算盈利。
+    def buy_longtime(self, fid, begin_date, end_date, n_pe=2, n_price=4, fee=0, base=100):
+        """ 长期购买一段时间，用于测试。默认买100块钱。以最后一天累计净值为基准计算盈利。
+            fee=申购费率*100，在实测中基本可以忽略费率对收益的影响。
         """
         days = (end_date - begin_date).days
         b_capital = 0
@@ -244,44 +246,18 @@ class FundValue():
             b_capital = b_capital + res[0]
             b_amount = b_amount + res[1]
         fprice = float(self.f_info[fid].get(self.get_today(end_date))[1])
-        aprice = 0 if b_capital==0 else round(b_capital/b_amount, 4)
-        win = 0 if b_capital==0 else (b_amount * fprice - b_capital) * 100 / b_capital
+        avg_price = 0 if b_capital==0 else round(b_capital/b_amount, 4)
+        win = 0 if b_capital==0 else (b_amount*fprice-b_capital-b_capital*fee/100) * 100 / b_capital
         win = str(round(win, 2)) + '%'
-        return (round(b_capital,2), round(b_amount,2), aprice, win)
+        return (round(b_capital,2), round(b_amount,2), avg_price, win)
 
     def bs_longtime(self, fid, begin_date, end_date, n_pe=2, n_price=4, base=100):
         """ 长期购买一段时间，用于测试。默认买100块钱。超过70水位线则卖出。
             为增加盈利，会将当前已赎回的钱再去申购基金，由于很难确定申购额度，导致最终盈利很难超过长期持有。
-            回测结果显示如不缺钱，做波段不如长期买着不再赎回。
-            不考虑申赎费，长期看回报率年化8%。
-            另外，每天无脑定投固定金额毫无意义。
+            回测结果显示做波段不如长期持有。
+            每天无脑定投固定金额毫无意义。
         """
-        days = (end_date - begin_date).days
-        sum_capital = 0
-        earn_capital = 0
-        b_capital = 0.01
-        b_amount = 0
-        dt = begin_date
-        for i in range(days):
-            if b_amount > 0 and self.peinfo.get(self.get_yesterday(dt)) > self.get_nwater(dt, 70):
-                earn_capital = earn_capital + self.f_info[fid].get(self.get_yesterday(dt)) * b_amount
-                print(dt, self.f_info[fid].get(self.get_yesterday(dt)), earn_capital)
-                b_amount = 0
-                b_capital = 0.01
-            res = self.buy_1day(fid, dt, n_pe, n_price, base)
-            dt = dt + datetime.timedelta(days=1)
-            sum_capital = sum_capital + res[0]
-            b_capital = b_capital + res[0]
-            b_amount = b_amount + res[1]
-            # 将已赎回的钱再去申购，每次申购额是当前申购额的3倍（此数字只适用于100038）。
-            if earn_capital > res[0]*3 :
-                b_capital = b_capital + res[0]*3
-                b_amount = b_amount + res[1]*3
-                earn_capital = earn_capital - res[0]*3
-        fprice = float(self.f_info[fid].get(self.get_today(end_date)))
-        win = (b_amount * fprice + earn_capital - sum_capital) * 100 / sum_capital
-        win = str(round(win, 2)) + '%'
-        return (round(sum_capital,2), round(b_amount,2), round(earn_capital, 2), round(b_amount*fprice, 2), win)
+        pass
 
 
 if __name__ == '__main__':
@@ -289,15 +265,19 @@ if __name__ == '__main__':
 
     #fv.init_s50_peinfo()
     #t = 2016
+    #fee = 0.1
 
     fv.init_hs300_peinfo()
     t = 2011
+    fee = 0.15
 
     #fv.init_hsbonus_peinfo()
     #t = 2011
+    #fee = 0.12
 
     #fv.init_gem_peinfo()
     #t = 2018
+    #fee = 0.12
 
     fid = fv.fids[0]
     fv.init_f_info2(fid)
@@ -311,5 +291,5 @@ if __name__ == '__main__':
     bd = datetime.datetime(t, 1, 1)
     ed = datetime.datetime(2020, 1, 1)
     #print(fv.bs_longtime(fid, bd, ed, 2, 4))
-    print(fv.buy_longtime(fid, bd, ed, 2, 4))
+    print(fv.buy_longtime(fid, bd, ed, 2, 4, fee))
     print(fv.buy_1day(fid, n_pe=2, n_price=4, base=100))
