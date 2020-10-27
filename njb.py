af20cd451fe110a8238dff454f934b969516a7c9
tuo.li <tuo.li@howbuy.com>
2020-10-27 08:45:39 +0800

    更新牛基宝净值。

--- njb.py
+++ njb.py
@@ -129,6 +129,7 @@ if __name__ == '__main__':
     today = n.buy_1day(n=10)
     buy_log = n.get_buylog()
     buy_log.append(today['capital'])
+    print(buy_log)
     print(today)
     print(n.get_buylog_water(buy_log))
 
